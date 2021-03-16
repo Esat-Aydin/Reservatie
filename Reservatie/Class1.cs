b65064d6399a0f28253266d7b25c92182cec2624
Esat-Aydin <esat61_2001@hotmail.com>
2021-03-16 14:03:52 +0100

    <3

---
+++ Reservatie/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservatie
+{
+    class Class1
+    {
+        static void dog(string[] args)
+        {
+            Console.WriteLine("Hello World!ez");
+        }
+    }
+}
+
+
