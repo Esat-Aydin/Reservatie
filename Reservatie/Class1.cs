0834e387e4869e299b8cfe63fa2c34799d69c68b
abdelilahtsouli <79145785+abdelilahtsouli@users.noreply.github.com>
2021-03-30 12:23:03 +0200

    Update Class1.cs

--- Reservatie/Class1.cs
+++ Reservatie/Class1.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace Reservatie
 {
-    class Class1
+    class Info
     {
-        public static void dog()
+        
+        public static void Reservering(string[] args)
         {
-            Console.WriteLine("Hello World!ez");
-        }
+            Console.WriteLine("Kies een genre uit de volgende lijst:");
+            Console.WriteLine("- Action");
+            Console.WriteLine("- Family");
+            Console.WriteLine("- Comedy");
+            Console.WriteLine("- Sci-Fi");
+            Console.WriteLine("- Romantic");
+            Console.WriteLine("- Thriller");
+            Console.WriteLine("Type hier de uitgekozen genre in en toets dan op ENTER: ");
+            string genre = Console.ReadLine();
+            if(genre == "Action"){
+                Console.WriteLine("De volgende Action films worden actueel getoond:");
+            }
+       }
     }
 }
 
