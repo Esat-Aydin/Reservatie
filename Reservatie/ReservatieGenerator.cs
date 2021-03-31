d7a19ddeb134ed1be8cd9e84e3df6a8c54350ba3
Wouter van Krugten <1010746@hr.nl>
2021-03-31 18:41:57 +0200

    .

--- Reservatie/ReservatieGenerator.cs
+++ Reservatie/ReservatieGenerator.cs
@@ -2,7 +2,7 @@
 using System.Threading;
 using MailKit.Net.Smtp;
 using MimeKit;
-namespace Reservering_bevestiging
+namespace Cinema
 {
     class Program
     {
@@ -14,30 +14,50 @@ namespace Reservering_bevestiging
             Console.WriteLine("Selecteer '2' om te zoeken op een specifieke film.");
             Console.WriteLine("Selecteer '3' om uw reservering te bekijken.");
             var Start_options = Console.ReadLine();
-            if (Start_options == "1"){
-            
+            if (Start_options == "1") {
+
+
             }
-            else if (Start_options == "2"){
-            
-            
+            else if (Start_options == "2") {
+
+
             }
-            else if (Start_options == "3"){
+            else if (Start_options == "3") {
                 Console.Write("Voer hier uw reservatie code in:");
-                string Reservatie_code = Console.ReadLine();
-                 
-            
-            
-            
+                var Reservatie_code = Console.ReadLine();
+
+
+
+
             }
-
-
-
+           
+            //Eventuele snacks tijdens het reserveren
+            Console.WriteLine("Zou u ook alvast snacks willen bestellen voor bij de film?");
+            Console.WriteLine("Door online de snacks te reserveren krijgt u 15% korting op het gehele bedrag.");
+            Console.WriteLine("Toets 'JA' als u online snacks wilt bestellen, toets 'NEE' als u dit niet wilt.");
+            string Online_snacks = Console.ReadLine();
+            string Online_snacks_secondchange = null;
+            if (Online_snacks == "NEE")
+                {
+                    Console.WriteLine("U heeft er voor gekozen om geen snacks te bestellen.");
+                    Console.WriteLine("Weet u het zeker? Toets 'JA' om door te gaan en 'NEE' om het overzicht te bekijken met de snacks.");
+                    Online_snacks_secondchange = Console.ReadLine();
+                    
+            }
+            if (Online_snacks == "JA" || Online_snacks_secondchange == "NEE")
+            {
+                    Console.WriteLine("Hieronder vindt u de lijst met de verkrijgbare snacks en dranken.");
+                    Console.WriteLine("");
+            }
+            
             // informatie voor eventueel mailen reservatie code.
-            Console.WriteLine("Om te kunnen reserveren hebben wij een aantal gegevens van u nodig.");
+            Console.WriteLine("Om te kunnen reserveren hebben wij uw naam en emailadres van u nodig.");
             Console.Write("Naam: ");
             string Naam_klant = Console.ReadLine();
             Console.Write("Email adress: ");
             string Naam_email = Console.ReadLine();
+            // Eventuele betaal methode?
+
             // Einde reserveren.
             Console.WriteLine("Bedankt voor het reserveren!");
             Console.WriteLine("Een ogenblik geduld alstublieft uw reservatie code wordt geladen.");
@@ -59,7 +79,7 @@ namespace Reservering_bevestiging
             Console.WriteLine("Zou je een bevestiging in je mail willen ontvangen?");
             Console.WriteLine("Toets 'JA' als je een bevestinging wil ontvangen toets 'NEE' als je geen bevestiging per mail wil ontvangen.");
             // Email bevestiging.
-            var Mail_Bevestiging = Console.ReadLine();
+            string Mail_Bevestiging = Console.ReadLine();
            
             if (Mail_Bevestiging == "JA")
             {
@@ -77,7 +97,7 @@ namespace Reservering_bevestiging
                     message.Body = new TextPart("plain")
                     {
                         Text = @"hallo,
-Bedankt voor het reserveren via onze bioscoop applicatie.
+Bedankt voor het reserveren via onze bioscoop.
 Hieronder vind je de reservatie code.
 Reservatie code: " + Reservatiecode
 
